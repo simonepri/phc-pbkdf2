7516a17b67dd5dd4ff0eaf6f2b46ad72b2ad8ab8
simonepri <simonepri@outlook.com>
2018-03-30 15:41:11 +0200

    Update bench

--- bench.js
+++ bench.js
@@ -14,30 +14,34 @@ async function bench(hpass, vpass, iterations) {
 }
 
 Promise.resolve()
+  // Default configs
+  .then(() => bench('r9(yaV@L', 'r9(yaV@L'))
+  .then(results => {
+    console.log('► CMD:', results.cmd);
+    console.log(results.stdout);
+  })
+
+  // Custom Iterations
   .then(() => bench('r9(yaV@L', 'r9(yaV@L', 1000))
   .then(results => {
     console.log('► CMD:', results.cmd);
     console.log(results.stdout);
   })
-
   .then(() => bench('r9(yaV@L', 'r9(yaV@L', 10000))
   .then(results => {
     console.log('► CMD:', results.cmd);
     console.log(results.stdout);
   })
-
   .then(() => bench('r9(yaV@L', 'r9(yaV@L', 100000))
   .then(results => {
     console.log('► CMD:', results.cmd);
     console.log(results.stdout);
   })
-
   .then(() => bench('r9(yaV@L', 'r9(yaV@L', 250000))
   .then(results => {
     console.log('► CMD:', results.cmd);
     console.log(results.stdout);
   })
-
   .then(() => bench('r9(yaV@L', 'r9(yaV@L', 500000))
   .then(results => {
     console.log('► CMD:', results.cmd);
